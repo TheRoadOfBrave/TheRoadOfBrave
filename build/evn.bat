f9b72b5eb4089a57c20dc864aec323df6243470b
unknown <JuLi@APSHADESK637.aaa.ad.ea.com>
2014-10-08 18:19:38 +0800

    create build files

---
+++ build/evn.bat
@@ -0,0 +1,6 @@
+set ANT_HOME=C:\pogo\tools\ant-1.7
+set ANT_OPTS=-Xmx128m
+set JAVA_HOME=D:\Java\jdk1.6.0_24
+set CLASSPATH=.;%JAVA_HOME%\bin;%JAVA_HOME%\lib
+set PATH=%JAVA_HOME%\jre\bin;%JAVA_HOME%\bin;%ANT_HOME%\bin;C:\pogo\tools\flex-sdk-4.0\bin;%PATH%
+cmd
